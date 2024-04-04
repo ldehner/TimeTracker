221ee2a5c07f03ce34e2acefc9907258f6d31fd8
Linus Dehner <dev@linusdehner.at>
2024-04-04 19:56:56 +0200

    added date read and basic form

--- TimeTracker/src/app/app.component.ts
+++ TimeTracker/src/app/app.component.ts
@@ -1,13 +1,257 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { Observable, map, startWith } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+import { catchError, of, firstValueFrom } from 'rxjs';
+import { ENV } from '../../envitonment';
+import { AsyncPipe } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
+import {
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MatMomentDateModule,
+  MomentDateAdapter,
+} from '@angular/material-moment-adapter';
 
+import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+const MY_DATE_FORMAT = {
+  parse: {
+    dateInput: 'DD/MM/YYYY', // this is how your date will be parsed from Input
+  },
+  display: {
+    dateInput: 'DD. MMMM', // this is how your date will get displayed on the Input
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+const headers = { Authorization: ENV.API_KEY };
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMAT },
+  ],
+  imports: [
+    RouterOutlet,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatMomentDateModule,
+    MatButtonModule,
+    HttpClientModule,
+    AsyncPipe,
+    NgFor,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'TimeTracker';
+  constructor(private http: HttpClient) {}
+  // OnInit
+  ngOnInit() {
+    this.fetchProjects();
+    this.setupFilters();
+  }
+  // states
+  isLoading = false;
+  showNotification = false;
+  notification = '';
+  isError = false;
+  isSuccess = false;
+
+  // form
+  date = new FormControl(new Date());
+  kommentar = '';
+  bezeichnung = '';
+  status = false;
+  from = this.setCurrentTime();
+  to = this.setCurrentTime();
+  selectedProject: any;
+  selectedTask: [string, string] = ['', ''];
+
+  // ===============================================================================
+  /**
+   * Filters
+   */
+  projectControl = new FormControl();
+  taskControl = new FormControl();
+  projects: [string, string][] = [];
+  tasks: [string, string][] = [];
+  filteredProjects: Observable<[string, string][]> = new Observable();
+  filteredTasks: Observable<[string, string][]> = new Observable();
+  private setupFilters() {
+    this.filteredProjects = this.projectControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this.filter('projects', value))
+    );
+
+    this.filteredTasks = this.taskControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this.filter('tasks', value))
+    );
+  }
+  private filter(
+    arrayName: 'projects' | 'tasks',
+    value: string
+  ): [string, string][] {
+    const filterValue = value.toLowerCase();
+    return this[arrayName].filter((option) =>
+      option[1].toLowerCase().includes(filterValue)
+    );
+  }
+
+  // ===============================================================================
+  /**
+   * Helper Functions
+   */
+  public getDate() {
+    let tmpDate = '';
+    if (this.date.value instanceof Date === false) {
+      const tmp = (this.date.value as any)._i;
+      tmpDate =
+        tmp.year +
+        '-' +
+        this.leadingZero(tmp.month) +
+        '-' +
+        this.leadingZero(tmp.date);
+    } else {
+      tmpDate = this.date.value.toISOString().split('T')[0];
+    }
+    return tmpDate;
+  }
+  setCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+
+  private leadingZero(value: number) {
+    return value < 10 ? '0' + value : value;
+  }
+  log() {
+    console.log('log');
+    console.log(this.selectedProject);
+  }
+
+  // ===============================================================================
+  /**
+   * Handlers
+   */
+
+  public fetchProjects() {
+    this.getProjects().then((projects: any) => {
+      if (projects === null) {
+        this.notification = 'Fehler beim Abrufen der Projekte';
+        this.isSuccess = false;
+        this.isError = true;
+        this.showNotification = true;
+        return;
+      }
+      projects.forEach((project: any) => {
+        this.projects.push([project.id, project.name]);
+      });
+    });
+  }
+
+  public projectSelected(id: string) {
+    this.getTasks({ project: id }).then((tasks: any) => {
+      if (tasks === null) {
+        this.notification = 'Feher beim Abrufen der Tasks';
+        this.isSuccess = false;
+        this.isError = true;
+        this.showNotification = true;
+        return;
+      }
+      tasks.forEach((task: any) => {
+        this.tasks.push([task.id, task.name]);
+      });
+    });
+  }
+
+  public async save() {
+    this.isLoading = true;
+    const body = {
+      date: this.getDate(),
+      from: this.from,
+      to: this.to,
+      project: this.selectedProject,
+      task: this.selectedTask[0],
+      comment: this.kommentar,
+      status: this.status,
+    };
+    this.postLog(body).then((response: any) => {
+      if (response === null) {
+        this.notification = 'Fehler beim Speichern des Eintrags';
+        this.isSuccess = false;
+        this.isError = true;
+        this.showNotification = true;
+        this.isLoading = false;
+        return;
+      }
+      this.notification = 'Eintrag erfolgreich gespeichert';
+      this.isSuccess = true;
+      this.isError = false;
+      this.showNotification = true;
+      this.isLoading = false;
+    });
+  }
+
+  // ===============================================================================
+  /**
+   * API Calls
+   */
+  async getProjects() {
+    const observable = this.http.get(ENV.URL + '/projects', { headers }).pipe(
+      catchError(() => {
+        return of(null);
+      })
+    );
+    return await firstValueFrom(observable);
+  }
+  async getTasks(queryParams: { [key: string]: string }) {
+    let params = new HttpParams();
+    for (const key of Object.keys(queryParams)) {
+      params = params.append(key, queryParams[key]);
+    }
+    const options = { params: params, headers: headers };
+    const observable = this.http.get(ENV.URL + '/tasks', options).pipe(
+      catchError(() => {
+        return of(null);
+      })
+    );
+    return await firstValueFrom(observable);
+  }
+  async postLog(body: { [key: string]: any }) {
+    const observable = this.http.post('/log', body, { headers }).pipe(
+      catchError(() => {
+        return of(null);
+      })
+    );
+    return firstValueFrom(observable);
+  }
 }
